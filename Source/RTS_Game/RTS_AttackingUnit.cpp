aff1689e4d2fcea17d120c158e0b76c2c0d9becc
Christopher Laganella <Luckylags13@gmail.com>
2018-09-06 01:03:25 -0400

    Added attacking unit class to be inherited by any unit that can target other units with an attack or heal

---
+++ Source/RTS_Game/RTS_AttackingUnit.cpp
@@ -0,0 +1,7 @@
+// Christopher Laganella - Ramapo College of New Jersey senior project
+
+#include "RTS_AttackingUnit.h"
+
+
+
+
---
+++ Source/RTS_Game/RTS_AttackingUnit.h
@@ -0,0 +1,20 @@
+// Christopher Laganella - Ramapo College of New Jersey senior project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTS_Unit.h"
+#include "RTS_AttackingUnit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_GAME_API ARTS_AttackingUnit : public ARTS_Unit
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
